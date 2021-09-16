c1acb2f38b82938c65f88a3927f23feb4a85dbe8
aleksandra-kim <aleksandra.kim@icloud.com>
2021-09-16 13:32:18 +0200

    replaced setup.py with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,51 +1,21 @@
+"""
+    Setup file for my_project.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 4.0.2.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
 from setuptools import setup
-import os
 
-packages = []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('bw2_aware'):
-    # Ignore dirnames that start with '.'
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-
-setup(
-    name='bw2_aware',
-    version="0.2.1",
-    packages=packages,
-    author="Chris Mutel",
-    author_email="cmutel@gmail.com",
-    license=open('LICENSE.txt', encoding='utf-8').read(),
-    install_requires=[
-        "brightway2",
-        "bw2regional",
-        "wrapt",
-    ],
-    package_data={'bw2_aware': ["data/*.*"]},
-    url="https://github.com/cmutel/bw2_aware",
-    long_description_content_type="text/markdown",
-    long_description=open("README.md").read(),
-    description=('AWARE regionalized LCIA method for Brightway2'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Visualization',
-    ],
-)
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
